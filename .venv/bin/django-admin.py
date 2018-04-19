b3138778118b33d982ee2b489a56f5e06f2f254e
IP <progressbg.python.course@gmail.com>
2018-04-19 12:00:43 +0300

    Initial commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/data/projects/www/wwwcourses.github.io/ProgressBG-Python/projects/DjangoTests/heroku-django-template/.venv/bin/python3.6m
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
